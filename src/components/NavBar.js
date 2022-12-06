08f51ce165ed6e9cf8004c5d726ec6751489525d
MARTIN HANSEN <marrtt1234@gmail.com>
2022-12-06 00:48:20 +0000

    test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
-// import { Navbar, NavItem } from 'react-bootstrap';
-// import { Link } from "react-router-dom";
+// import { Nav } from 'react-bootstrap';
+import { Navbar, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import '../css/NavBar.css';
 
 class NavBar extends React.Component {
   render() {
     return (
-      <Nav activeKey="/">
-        <Nav.Item>
-          <Nav.Link to="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link to="/date">Plan a Date</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link to="/profile">Profile</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link to="/about">About Us</Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Navbar>
+        <NavItem>
+          <Link to='/'>Home</Link>
+          <Link to='/date'>Plan a Date</Link>
+          <Link to='/profile'>Profile</Link>
+          <Link to='/about'>About Us</Link>
+        </NavItem>
+      </Navbar>
+      // <Nav activeKey='/'>
+      //   <Nav.Item>
+      //     <Nav.Link to='/'>Home</Nav.Link>
+      //   </Nav.Item>
+      //   <Nav.Item>
+      //     <Nav.Link to='/date'>Plan a Date</Nav.Link>
+      //   </Nav.Item>
+      //   <Nav.Item>
+      //     <Nav.Link to='/profile'>Profile</Nav.Link>
+      //   </Nav.Item>
+      //   <Nav.Item>
+      //     <Nav.Link to='/about'>About Us</Nav.Link>
+      //   </Nav.Item>
+      // </Nav>
     );
   }
 }
